5327064f8e294b179acd19683e9d2a5b03ab9373
AbdulRehman028 <164177410+AbdulRehman028@users.noreply.github.com>
2025-07-22 15:59:46 +0500

    First Commit

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,3 +1,60 @@
 const appname = "eMart";
 const appversion = "Version 1.0.0";
-const credits = "@Baaba Devs";
+const credits = "@Dofday Solutions|@Abdulrehman";
+const email = "Email";
+const emailHint = "admin@gmail.com";
+const password = "Password";
+const passwordHint = "********";
+const retypePassword = "Retype Password";
+const name = "Name";
+const nameHint = "Dofay";
+const forgotPassword = "Forgot Password?";
+const login = "Log in";
+const logout = "Logout";
+const signUp = "Sign Up";
+const createNewAccount = "or, create a new account";
+const loginWith = "Log in with";
+const privacyPolicy = "Privacy Policy";
+const termAndCond = "Terms and Conditions";
+const alreadyHaveAccount = "Already have an account? ";
+const home = "Home",
+    categories = "Categories",
+    cart = "Cart",
+    account = "Account";
+
+const searchAnything = "Search anything...",
+    todayDeal = "Today's Deal",
+    flashSale = "Flash Sale",
+    topSellers = "Top Sellers",
+    brand = "Brand",
+    topCategories = "Top Categories",
+    womenDress = "Women Dress",
+    girlWatches = "Girls Watches",
+    mobilePhone = "Mobile Phone",
+    boysGlasses = "Boys Glasses",
+    tShirt = "T-Shirts",
+    girlsDress = "Girls Dress",
+    featuredCategories = "Featured Categories";
+
+const featuredProduct = "Featured Product";
+
+const womenClothing = "Women Clothing",
+    menClothingFashion = "Men Clothing & Fashion",
+    compAccess = "Computer & Accessories",
+    automobile = "Automobile",
+    kidToys = "Kids Toys",
+    sports = "Sports",
+    jewelery = "Jewelery",
+    cellphone = "Cellphone",
+    furniture = "Furniture";
+
+const video = "Video",
+    reviews = "Reviews",
+    sellerPolicy = "Seller Policy",
+    returnPolicy = "Return Policy",
+    supportPolicy = "Support Policy",
+productYouLike = "Products You may Like";
+
+const wishlist = "My Wishlist",
+    myOrders = "My Orders",
+    messages = "Messages";
